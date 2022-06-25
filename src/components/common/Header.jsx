faac5dedcb3c595863f2a2799b63a1f7901c37fc
2Junsu <ijh1205@naver.com>
2022-06-25 17:02:27 +0900

    Feat: 홈 화면 라우팅

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import styled from "styled-components";
 import hamburger from "../../assets/imgs/img-hamburger.png";
 import logo from "../../assets/imgs/img-logo.png";
+import { Link } from "react-router-dom";
 
 const Header = () => {
     return (
@@ -13,7 +14,9 @@ const Header = () => {
                         src={hamburger}
                         alt="hamburger-img"
                     />
-                    <img className="logo-img" src={logo} alt="logo" />
+                    <Link to="/">
+                        <img className="logo-img" src={logo} alt="logo" />
+                    </Link>
                 </div>
                 <ul className="menu">
                     <li>채용</li>
