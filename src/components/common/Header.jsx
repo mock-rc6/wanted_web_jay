620e4dff3fdaf376b22960af91fba1b1f5417a83
2Junsu <ijh1205@naver.com>
2022-06-29 16:31:43 +0900

    Design: Header padding 수정

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -12,60 +12,64 @@ const Header = () => {
 
     return (
         <Wrap>
-            <nav className="main-bar">
-                <div>
-                    <img
-                        className="hamburger-img"
-                        src={hamburger}
-                        alt="hamburger-img"
-                    />
-                    <Link to="/">
-                        <img className="logo-img" src={logo} alt="logo" />
-                    </Link>
-                </div>
-                <ul className="menu">
-                    <li>채용</li>
-                    <li>이벤트</li>
-                    <li>직군별 연봉</li>
-                    <li>이력서</li>
-                    <li>커뮤니티</li>
-                    <li>프리랜서</li>
-                    <li>AI 합격예측</li>
-                </ul>
-                <aside className="header-aside">
-                    <button>
-                        <svg
-                            xmlns="https://www.w3.org/2000/svg"
-                            xmlnsXlink="https://www.w3.org/1999/xlink"
-                            width="18"
-                            height="18"
-                            viewBox="0 0 18 18">
-                            <defs>
-                                <path
-                                    id="qt2dnsql4a"
-                                    d="M15.727 17.273a.563.563 0 10.796-.796l-4.875-4.875-.19-.165a.563.563 0 00-.764.028 5.063 5.063 0 111.261-2.068.562.562 0 101.073.338 6.188 6.188 0 10-1.943 2.894l4.642 4.644z"></path>
-                            </defs>
-                            <g fill="none" fillRule="evenodd">
-                                <use
-                                    fill="#333"
-                                    fillRule="nonzero"
-                                    stroke="#333"
-                                    strokeWidth=".3"
-                                    xlinkHref="#qt2dnsql4a"></use>
-                            </g>
-                        </svg>
-                    </button>
-                    <button
-                        onClick={() => {
-                            if (!openSignupDetailModal)
-                                setOpenSignupModal(true);
-                        }}>
-                        회원가입/로그인
-                    </button>
-                    <span>|</span>
-                    <button className="btn-company-service">기업 서비스</button>
-                </aside>
-            </nav>
+            <div>
+                <nav className="main-bar">
+                    <div>
+                        <img
+                            className="hamburger-img"
+                            src={hamburger}
+                            alt="hamburger-img"
+                        />
+                        <Link to="/">
+                            <img className="logo-img" src={logo} alt="logo" />
+                        </Link>
+                    </div>
+                    <ul className="menu">
+                        <li>채용</li>
+                        <li>이벤트</li>
+                        <li>직군별 연봉</li>
+                        <li>이력서</li>
+                        <li>커뮤니티</li>
+                        <li>프리랜서</li>
+                        <li>AI 합격예측</li>
+                    </ul>
+                    <aside className="header-aside">
+                        <button>
+                            <svg
+                                xmlns="https://www.w3.org/2000/svg"
+                                xmlnsXlink="https://www.w3.org/1999/xlink"
+                                width="18"
+                                height="18"
+                                viewBox="0 0 18 18">
+                                <defs>
+                                    <path
+                                        id="qt2dnsql4a"
+                                        d="M15.727 17.273a.563.563 0 10.796-.796l-4.875-4.875-.19-.165a.563.563 0 00-.764.028 5.063 5.063 0 111.261-2.068.562.562 0 101.073.338 6.188 6.188 0 10-1.943 2.894l4.642 4.644z"></path>
+                                </defs>
+                                <g fill="none" fillRule="evenodd">
+                                    <use
+                                        fill="#333"
+                                        fillRule="nonzero"
+                                        stroke="#333"
+                                        strokeWidth=".3"
+                                        xlinkHref="#qt2dnsql4a"></use>
+                                </g>
+                            </svg>
+                        </button>
+                        <button
+                            onClick={() => {
+                                if (!openSignupDetailModal)
+                                    setOpenSignupModal(true);
+                            }}>
+                            회원가입/로그인
+                        </button>
+                        <span>|</span>
+                        <button className="btn-company-service">
+                            기업 서비스
+                        </button>
+                    </aside>
+                </nav>
+            </div>
             <Signup
                 width={400}
                 modalStatus={openSignupModal}
@@ -86,7 +90,14 @@ const Header = () => {
 };
 
 const Wrap = styled.div`
-    padding: 0px 10%;
+    & > div {
+        width: 80%;
+    }
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 100%;
     height: 50px;
     border-bottom: 1px solid #cccccc;
     font-size: 14px;
@@ -96,6 +107,7 @@ const Wrap = styled.div`
     background-color: #fff;
 
     .main-bar {
+        margin: 0 auto;
         display: flex;
         align-items: center;
         justify-content: space-between;
