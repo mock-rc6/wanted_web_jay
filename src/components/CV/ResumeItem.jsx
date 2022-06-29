cac47027e71e5a8583e36661ac5429188b9b9128
2Junsu <ijh1205@naver.com>
2022-06-29 19:13:11 +0900

    Design: 이력서 리스트 페이지 UI

---
+++ src/components/CV/ResumeItem.jsx
@@ -0,0 +1,113 @@
+import React from "react";
+import styled from "styled-components";
+import { HiDotsVertical } from "react-icons/hi";
+
+const ResumeItem = () => {
+    return (
+        <Wrap>
+            <Title>
+                <h3>이준수 1</h3>
+                <p>2022.06.28</p>
+                <p className="match">매치업 이력서</p>
+            </Title>
+            <Info>
+                <div className="lang">한</div>
+                <span>작성 중</span>
+                <div className="dropdown">
+                    <button>
+                        <HiDotsVertical style={{ width: 24, height: 24 }} />
+                    </button>
+                </div>
+            </Info>
+        </Wrap>
+    );
+};
+const Wrap = styled.div`
+    height: 190px;
+    width: calc(25% - 20px);
+    margin-bottom: 20px;
+    margin-right: 20px;
+    position: relative;
+    border: 1px solid #dbdbdb;
+    background-color: #fff;
+    box-sizing: border-box;
+    cursor: pointer;
+`;
+const Title = styled.div`
+    padding: 20px;
+    font-size: 16px;
+    font-weight: 500;
+    text-align: left;
+    letter-spacing: normal;
+
+    & h3 {
+        margin: 0;
+        font-size: 18px;
+        font-weight: 600;
+        line-height: 1.33;
+        max-height: 46px;
+        max-width: 100%;
+        letter-spacing: normal;
+        text-align: left;
+        color: #999;
+        overflow: hidden;
+        word-break: break-all;
+    }
+
+    & p {
+        margin: 0;
+        margin-top: 5px;
+        color: #999;
+    }
+
+    .match {
+        color: #36f;
+    }
+`;
+const Info = styled.div`
+    position: absolute;
+    bottom: 0;
+    height: 41px;
+    width: 100%;
+    display: flex;
+    align-items: center;
+    padding: 0 12px 0 20px;
+    border-top: 1px solid #e0e0e0;
+    box-sizing: border-box;
+
+    .lang {
+        width: 20px;
+        height: 20px;
+        border-radius: 2px;
+        border: 1px solid #999;
+        color: #999;
+        text-align: center;
+        font-size: 12px;
+        line-height: 20px;
+        font-weight: 600;
+        margin-right: 10px;
+    }
+
+    & > span {
+        color: #999;
+        font-size: 16px;
+        font-weight: 600;
+        line-height: 20px;
+    }
+
+    .dropdown {
+        margin-left: auto;
+        & > button {
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            border: 0;
+            background: none;
+            width: 24px;
+            height: 100%;
+            color: #76797e;
+            cursor: pointer;
+        }
+    }
+`;
+export default ResumeItem;
---
+++ src/pages/CV/CVList.jsx
@@ -0,0 +1,144 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+import Header from "../../components/common/Header";
+import { IoMdInformationCircleOutline } from "react-icons/io";
+import { TbCopy } from "react-icons/tb";
+import { BiUpload } from "react-icons/bi";
+import ResumeItem from "../../components/CV/ResumeItem";
+
+const mock = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const CVList = () => {
+    return (
+        <Wrap>
+            <Header />
+            <div className="resume-list">
+                <div className="resume-list-header">
+                    <h4>최근 문서</h4>
+                    <Link
+                        to="/cv/intro"
+                        style={{ textDecoration: "none", color: "#36f" }}>
+                        <div className="link">
+                            <span>원티드 이력서 소개</span>
+                            <IoMdInformationCircleOutline
+                                style={{ marginLeft: 8, width: 20, height: 20 }}
+                            />
+                        </div>
+                    </Link>
+                </div>
+                <div className="resume-list-list">
+                    <ResumeAdd className="resume-item">
+                        <div>
+                            <TbCopy
+                                style={{
+                                    width: 26,
+                                    height: 26,
+                                    transform: "rotate(180deg)",
+                                    color: "white",
+                                }}
+                            />
+                        </div>
+                        <p>새 이력서 작성</p>
+                    </ResumeAdd>
+                    <ResumeUpload className="resume-item">
+                        <div>
+                            <BiUpload
+                                style={{ width: 26, height: 26, color: "#666" }}
+                            />
+                        </div>
+                        <p>파일 업로드</p>
+                    </ResumeUpload>
+                    {mock.map((data, idx) => (
+                        <ResumeItem key={idx} />
+                    ))}
+                </div>
+            </div>
+        </Wrap>
+    );
+};
+
+const Wrap = styled.div`
+    background-color: #f8f8fa;
+    height: 100vh;
+
+    .resume-list {
+        width: 80%;
+        margin: 0 auto;
+
+        .resume-list-header {
+            display: flex;
+            align-items: flex-end;
+            justify-content: space-between;
+            margin: 25px 0 5px;
+
+            & h4 {
+                font-size: 16px;
+                font-weight: 600;
+                line-height: 1.4;
+                padding: 15px 0;
+                margin: 0;
+                color: #333;
+                word-wrap: break-word;
+            }
+
+            .link {
+                padding: 15px 0;
+                display: flex;
+                align-items: center;
+
+                & span {
+                    font-size: 16px;
+                    font-weight: 600;
+                }
+            }
+        }
+
+        .resume-list-list {
+            display: flex;
+            justify-content: flex-start;
+            flex-wrap: wrap;
+            width: calc(100% + 20px);
+
+            .resume-item {
+                height: 190px;
+                width: calc(25% - 20px);
+                margin-bottom: 20px;
+                margin-right: 20px;
+                position: relative;
+                border: 1px solid #dbdbdb;
+                background-color: #fff;
+                box-sizing: border-box;
+                cursor: pointer;
+            }
+        }
+    }
+`;
+const ResumeAdd = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+
+    & > div {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        width: 74px;
+        height: 74px;
+        border-radius: 50%;
+        background-color: #36f;
+    }
+
+    & > p {
+        font-size: 16px;
+        font-weight: 600;
+        text-align: center;
+        margin: 20px 0 0;
+    }
+`;
+const ResumeUpload = styled(ResumeAdd)`
+    & > div {
+        background-color: #e1e2e3;
+    }
+`;
+export default CVList;
