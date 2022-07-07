668250c25598b672b24d1e6d1ae34645508ff2c7
2Junsu <ijh1205@naver.com>
2022-07-07 15:48:21 +0900

    Modal z-index 수정

--- src/components/common/Modal.jsx
+++ src/components/common/Modal.jsx
@@ -16,6 +16,7 @@ const Modal = (props) => {
                     top: 0,
                     left: 0,
                     backgroundColor: "rgba(0,0,0,0.5)",
+                    zIndex: 999,
                 },
                 content: {
                     padding: 0,
